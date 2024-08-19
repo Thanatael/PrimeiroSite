faa44d9589648f578311b13376c54f3da95ade78
Thanatael <william777200800@gmail.com>
2024-08-19 19:51:33 -0300

    socoroo

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $titulo = "teste"
+    $url_imagem1 = "caminho";
+    ?>
+    <h1><?php echo $titulo;?></h1>
+    <?=$url_imagem1;?>
+</body>
+</html>
